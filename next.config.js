bf8eae7c75892792e549d7ba12bbe90736729a97
Amauri Bechtold Junior <amauribechtoldjr@gmail.com>
2022-04-11 08:51:41 -0300

    formating tsconfig file

--- next.config.js
+++ next.config.js
@@ -7,9 +7,10 @@ const withImages = require('next-images')
 module.exports =
   withFrameworkConfig(
     withImages({
+      framework: {
+        name: 'shopify'
+      },
       esModule: true
     }
   )
 )
-
-console.log('sadfadsfasdf', JSON.stringify(module.exports, null, 2))
