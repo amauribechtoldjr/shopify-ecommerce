bdb634ceea343aa08e0d7c3660dd0b1bb41605ac
Amauri Bechtold Junior <amauribechtoldjr@gmail.com>
2022-04-14 12:23:32 -0300

    ProductCard with placeholder image

--- src/components/product/ProductCard/ProductCard.tsx
+++ src/components/product/ProductCard/ProductCard.tsx
@@ -1,15 +1,41 @@
 import { Product } from '@common/types/product'
 import Link from 'next/link'
+import Image from 'next/image'
+import * as S from './styles'
+import { FC, useCallback } from 'react'
 
 interface Props {
   product: Product
 }
 
-const ProductCard: React.FC<Props> = ({ product }) => {
+const PLACEHOLDER_IMAGE = '/product-placeholder.svg'
+
+const ProductCard: FC<Props> = ({ product }) => {
+  const renderProductImages = useCallback(() => {
+    if (product.images) {
+      return (
+        <Image
+          src={PLACEHOLDER_IMAGE}
+          alt={product.name}
+          width={540}
+          height={540}
+          quality="85"
+          layout="responsive"
+        />
+      )
+    }
+  }, [product.images])
+
   return (
-    <div>
-      <Link href={`/product/${product.slug}`}>{product.name}</Link>
-    </div>
+    <>
+      <Link href={`/product/${product.slug}`}>
+        <S.ProductCardContainer>
+          <h1>{product.name}</h1>
+          <span> R$45.50 </span>
+          {renderProductImages()}
+        </S.ProductCardContainer>
+      </Link>
+    </>
   )
 }
 
