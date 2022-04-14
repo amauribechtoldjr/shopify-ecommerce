d5406a0559a986d23a67217b1e90119b5132e88c
Amauri Bechtold Junior <amauribechtoldjr@gmail.com>
2022-04-14 13:27:51 -0300

    Size screens added to theme and media for grid

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,6 +4,13 @@ const theme = {
     text: '#222222',
     primary: '#D4F593',
     secundary: '#DED490'
+  },
+  screen_size: {
+    xm: '320px', // Mobile devices
+    sm: '481px', // iPads, Tablets
+    md: '769px', // Small screens, laptops
+    lg: '1025px', // Desktops, large screens
+    xl: '1201px' // Extra large screens, TV
   }
 }
 
