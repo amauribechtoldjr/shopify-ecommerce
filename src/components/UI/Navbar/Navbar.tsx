f5b3213a1b240dd50ef85d19bb495780e9c87c3c
Amauri Bechtold Junior <amauribechtoldjr@gmail.com>
2022-04-16 11:13:41 -0300

    Navbar links added

--- src/components/UI/Navbar/Navbar.tsx
+++ src/components/UI/Navbar/Navbar.tsx
@@ -7,21 +7,27 @@ const Navbar: FC = () => {
   return (
     <S.NavbarContainer>
       <S.NavbarContent>
-        <h1>Travesssa</h1>
-        <S.NavContainer>
-          <Link href="/products">
-            <S.NavLink>Produtos</S.NavLink>
+        <S.LinksContainer>
+          <Link href="/">
+            <S.NavLink>
+              <h1>TRAVESSA</h1>
+            </S.NavLink>
           </Link>
-          <Link href="/products">
-            <S.NavLink>Sobre</S.NavLink>
-          </Link>
-          <Link href="/products">
-            <S.NavLink>Coleções</S.NavLink>
-          </Link>
-          <Link href="/products">
-            <S.NavLink>Travesssuras</S.NavLink>
-          </Link>
-        </S.NavContainer>
+          <S.NavContainer>
+            <Link href="/products">
+              <S.NavLink>Produtos</S.NavLink>
+            </Link>
+            <Link href="/products">
+              <S.NavLink>Sobre</S.NavLink>
+            </Link>
+            <Link href="/products">
+              <S.NavLink>Coleções</S.NavLink>
+            </Link>
+            <Link href="/products">
+              <S.NavLink>Travesssuras</S.NavLink>
+            </Link>
+          </S.NavContainer>
+        </S.LinksContainer>
       </S.NavbarContent>
     </S.NavbarContainer>
   )
--- src/components/UI/Navbar/styles.ts
+++ src/components/UI/Navbar/styles.ts
@@ -4,15 +4,17 @@ export const NavbarContainer = styled.div`
   user-select: none;
 `
 export const NavbarContent = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-
   padding: 2em 0.8em;
 `
 
 export const NavContainer = styled.nav`
   font-size: 16px;
+`
+
+export const LinksContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `
 
 export const NavLink = styled.a`
@@ -21,5 +23,5 @@ export const NavLink = styled.a`
     cursor: pointer;
   }
 
-  margin: 0 0.6em;
+  margin-left: 0.6em;
 `
