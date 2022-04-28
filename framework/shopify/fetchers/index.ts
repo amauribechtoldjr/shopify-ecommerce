3701cf19421358e409d986e9dc0b19ce50de220d
Amauri Bechtold Junior <amauribechtoldjr@gmail.com>
2022-04-28 19:39:36 -0300

    Adding default export for fetchers

---
+++ framework/shopify/fetchers/index.ts
@@ -0,0 +1,3 @@
+export { default as getProduct } from './products/get-product'
+export { default as getAllProducts } from './products/get-all-products'
+export { default as getAllProductsPaths } from './products/get-all-products-paths'
