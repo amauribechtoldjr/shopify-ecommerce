6ba3ecbff79e29d5d1c9f9fcfc2f274cc42ed631
Amauri Bechtold Junior <amauribechtoldjr@gmail.com>
2022-05-11 13:55:08 -0300

    Normalizing options and variants

--- framework/shopify/normalize.ts
+++ framework/shopify/normalize.ts
@@ -1,4 +1,12 @@
-import { ImageEdge, MoneyV2, Product as ShopifyProduct } from '@framework/types'
+import {
+  ImageEdge,
+  MoneyV2,
+  Product as ShopifyProduct,
+  ProductOption,
+  ProductVariantConnection,
+  ProductVariantEdge,
+  SelectedOption
+} from '@framework/types'
 import { Product } from '@common/types/product'
 
 export function normalizeProductImages({ edges }: { edges: Array<ImageEdge> }) {
@@ -15,6 +23,74 @@ export function normalizeProductPrice({ currencyCode, amount }: MoneyV2) {
   }
 }
 
+export function normalizeProductOption({
+  id,
+  values,
+  name: displayName
+}: ProductOption) {
+  const normalized = {
+    id,
+    displayName,
+    values: values.map(value => {
+      let valueNormalized: any = {
+        label: value
+      }
+
+      if (displayName.match(/color?/gi)) {
+        valueNormalized = { ...valueNormalized, hexColor: value }
+      }
+
+      return valueNormalized
+    })
+  }
+
+  return normalized
+}
+
+export function normalizeProductVariant({ node }: ProductVariantEdge) {
+  const { id, selectedOptions, sku, title, priceV2, compareAtPriceV2 } = node
+
+  return {
+    id,
+    name: title,
+    sku: sku || id,
+    price: +priceV2?.amount,
+    listPrice: +compareAtPriceV2?.amount,
+    requiresShipping: true,
+    options: selectedOptions.map(({ name, value }: SelectedOption) => {
+      const option = normalizeProductOption({
+        id,
+        name,
+        values: [value]
+      })
+
+      return option
+    })
+  }
+}
+
+function getNormalizedOptionsIfExists(options: Array<ProductOption>) {
+  if (!options) {
+    return []
+  }
+
+  const normalizedOptions = options
+    .filter(o => o.name !== 'Title')
+    .map(o => normalizeProductOption(o))
+
+  return normalizedOptions
+}
+
+function getNormalizedVariansIfExists(variants: ProductVariantConnection) {
+  if (!variants?.edges) {
+    return []
+  }
+
+  const normalizedVariants = variants.edges.map(e => normalizeProductVariant(e))
+
+  return normalizedVariants
+}
+
 export function normalizeProduct(productNode: ShopifyProduct): Product {
   const {
     id,
@@ -24,6 +100,8 @@ export function normalizeProduct(productNode: ShopifyProduct): Product {
     description,
     images: imageConnection,
     priceRange,
+    options,
+    variants,
     ...rest
   } = productNode
 
@@ -36,6 +114,8 @@ export function normalizeProduct(productNode: ShopifyProduct): Product {
     slug: handle.replace(/^\/+|\/+$/g, ''),
     images: normalizeProductImages(imageConnection),
     price: normalizeProductPrice(priceRange.minVariantPrice),
+    options: getNormalizedOptionsIfExists(options),
+    variants: getNormalizedVariansIfExists(variants),
     ...rest
   }
 
