c251a787d2bf572a8d9ea1b275316ac599c9f274
Amauri Bechtold Junior <amauribechtoldjr@gmail.com>
2022-07-14 11:57:09 -0300

    Add checkout id to browser

--- framework/shopify/utils/create-checkout.ts
+++ framework/shopify/utils/create-checkout.ts
@@ -1,6 +1,12 @@
 import { ApiFetcher } from '@common/types/api'
+import {
+  SHOPIFY_CHECKOUT_ID_COOKIE,
+  SHOPIFY_CHECKOUT_URL_COOKIE,
+  SHOPIFY_COOKIE_EXPIRE
+} from '@framework/const'
 import { createCheckoutMutation } from '@framework/mutations'
 import { Checkout, CheckoutCreatePayload, Maybe } from '@framework/types'
+import Cookies from 'js-cookie'
 
 const createCheckout = async (
   fetch: ApiFetcher<{ checkoutCreate: CheckoutCreatePayload }>
@@ -10,8 +16,17 @@ const createCheckout = async (
   })
 
   const { checkout } = data.checkoutCreate
-
   const checkoutId = checkout?.id
+
+  if (checkoutId) {
+    const options = {
+      expires: SHOPIFY_COOKIE_EXPIRE
+    }
+
+    Cookies.set(SHOPIFY_CHECKOUT_ID_COOKIE, checkoutId, options)
+    Cookies.set(SHOPIFY_CHECKOUT_URL_COOKIE, checkout?.webUrl, options)
+    alert('123')
+  }
 
   return checkout
 }
